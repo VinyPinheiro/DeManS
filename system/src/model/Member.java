9c52906f4186f8aaaedfeb378a827c8e07a0f878
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-09-01 21:22:08 -0300

    Adicionando pasta src

---
+++ system/src/model/Member.java
@@ -0,0 +1,11 @@
+/*****************************
+ * Class name: Member (.java)
+ * 
+ * Purpose: Class to create a member type with your attributes.
+ *****************************/
+
+package model;
+
+public class Member {
+
+}
