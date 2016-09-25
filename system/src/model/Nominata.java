1469c90234509531bc14e3182685f589c773566f
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-09-25 17:37:11 -0300

    Create models to Nominata and Office

--- system/src/model/Nominata.java
+++ system/src/model/Nominata.java
@@ -1,16 +1,60 @@
 package model;
 
 import java.util.Vector;
+import exception.NominataException;
 
 public class Nominata {
-	
+
+	private Integer id;
 	private Vector<Office> nominata;
-	
-	public Nominata(){
+
+	private static final String REPEATED_POSITION = "Esse cargo já foi preenchido nesta nominata.";
+	private static final String INVALID_ID = "Id não pode ser negativo.";
+
+	public Nominata() throws NominataException {
 		nominata = new Vector<Office>();
+		setId(null);
 	}
-	
-	public void addOffice(Office office){
-		this.nominata.add(office);
+
+	/**
+	 * 
+	 * @param id
+	 *            integer, only positive numbers
+	 * @throws NominataException
+	 */
+	public Nominata(int id) throws NominataException {
+		nominata = new Vector<Office>();
+		setId(id);
+	}
+
+	public void addOffice(Office office) throws NominataException {
+		// Verify if office is repeated
+		for (Office members : nominata) {
+			if (members.getOffice().equals(office.getOffice())) {
+				throw new NominataException(REPEATED_POSITION);
+			}
+		}
+
+		nominata.addElement(office);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Vector<Office> getNominataList() {
+		return nominata;
+	}
+
+	private void setId(Integer id) throws NominataException {
+		if (id != null) {
+			if (id > 0) {
+				this.id = id;
+			} else {
+				throw new NominataException(INVALID_ID);
+			}
+		} else {
+			this.id = id;
+		}
 	}
 }
